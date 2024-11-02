dc15404b87154748ce1e6bde9634651e66b43256
jejelebos <124287951+jejelebos@users.noreply.github.com>
2024-11-02 18:01:20 +0100

    Update webhook.bat

--- webhook.bat
+++ webhook.bat
@@ -3,6 +3,7 @@
 REM enter the directory where your code is located
 cd C:\Users\%USERNAME%\Desktop || (
     echo Error: The specified path does not exist.
+    timeout /t 5 /nobreak >nul
     exit /b
 )
 
@@ -15,4 +16,11 @@ if %errorlevel% neq 0 (
     echo pystyle is already installed.
 )
 
+REM Clear the terminal before running webhook.py
+cls
+
 python webhook.py
+
+REM Wait before closing the terminal
+echo Press any key to exit...
+pause >nul
