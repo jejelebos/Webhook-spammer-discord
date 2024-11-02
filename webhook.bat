7f99112fa68a4526daaddb7d4f50e84f2f49c560
jejelebos <124287951+jejelebos@users.noreply.github.com>
2024-11-02 17:57:14 +0100

    Update webhook.bat

--- webhook.bat
+++ webhook.bat
@@ -1,8 +1,18 @@
 @echo off
 
 REM enter the directory where your code is located
-cd C:\Users\%USERNAME%\Desktop
+cd C:\Users\%USERNAME%\Desktop || (
+    echo Error: The specified path does not exist.
+    exit /b
+)
 
-pip install pystyle
+REM check if pystyle is already installed
+pip show pystyle >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pystyle...
+    pip install pystyle
+) else (
+    echo pystyle is already installed.
+)
+
 python webhook.py
-
